4a99a9c66db92f2450b6f5fe5cd3d6498191b721
maggiflo98 <maggiflo98@gmail.com>
2021-11-09 05:26:41 +0300

    setup for the covid- tracker

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>lets build a covid-19 tracker</h1>
+      {/* header */}
+      {/* select input field */}
+      {/* infoboxes */}
+      {/* infoboxes */}
+      {/* graph */}
+    </div>
+  );
+}
+
+export default App;
